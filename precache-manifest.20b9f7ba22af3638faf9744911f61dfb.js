b2e4af30f3c0acf8aa66c763e68ef60994107bdd
Amogha027 <aahalhalli@gmail.com>
2024-04-08 15:46:59 +0530

    Updates

---
+++ precache-manifest.20b9f7ba22af3638faf9744911f61dfb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3a2f1fe5b1bc614589f0a7eb1e06c2",
+    "url": "/vreqst-gh-pages/index.html"
+  },
+  {
+    "revision": "fab60288b43651d054cf",
+    "url": "/vreqst-gh-pages/static/css/main.246c8ac2.chunk.css"
+  },
+  {
+    "revision": "fdab5e61570bb0f4f7a3",
+    "url": "/vreqst-gh-pages/static/js/2.ab937bb4.chunk.js"
+  },
+  {
+    "revision": "6b647c612c8d9845700141d8247a2c3c",
+    "url": "/vreqst-gh-pages/static/js/2.ab937bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab60288b43651d054cf",
+    "url": "/vreqst-gh-pages/static/js/main.7959eaaf.chunk.js"
+  },
+  {
+    "revision": "c97bbad0b7cd3dfad00a",
+    "url": "/vreqst-gh-pages/static/js/runtime-main.7f06eeef.js"
+  },
+  {
+    "revision": "fdb795e970c9485b3ee804de0d66f5b0",
+    "url": "/vreqst-gh-pages/static/media/action.fdb795e9.txt"
+  },
+  {
+    "revision": "a80fba56cbac9db56dccf9701952c9b2",
+    "url": "/vreqst-gh-pages/static/media/asset.a80fba56.txt"
+  },
+  {
+    "revision": "a7a93f0d2970fbfd93b97af6e5df74f4",
+    "url": "/vreqst-gh-pages/static/media/scene.a7a93f0d.txt"
+  },
+  {
+    "revision": "44f05b74bb5bea1ce5dd308de65ed12a",
+    "url": "/vreqst-gh-pages/static/media/timeline.44f05b74.txt"
+  }
+]);
